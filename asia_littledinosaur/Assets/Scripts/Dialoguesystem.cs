ae0cad9ecd351248a90996698bfd9543966c65a2
immortalimpostor9634 <90890242+immortalimpostor9634@users.noreply.github.com>
2021-12-17 15:20:16 +0800

    對話測試

--- asia_littledinosaur/Assets/Scripts/Dialoguesystem.cs
+++ asia_littledinosaur/Assets/Scripts/Dialoguesystem.cs
@@ -15,6 +15,10 @@ public class Dialoguesystem : MonoBehaviour
     public GameObject goDialogue;
     [Header("對話內容")]
     public Text textContent;
+    [Header("對話完成圖示")]
+    public GameObject goTip;
+    [Header("對話按鍵")]
+    public KeyCode keyDialogue = KeyCode.Mouse0;
 
     private void Start()
     {
@@ -27,16 +31,26 @@ public class Dialoguesystem : MonoBehaviour
         string test2 = "今天的天氣真好~";
         string[] test = { test1,test2 };
 
-        textContent.text = "";    // 清除上次對話內容
         goDialogue.SetActive(true);   // 顯示對話物件
 
-        for (int j = 0; j < test.Length; j++) 
+        for (int j = 0; j < test.Length; j++)   // 遍尋所有對話
         {
-            for (int i = 0; i < test[j].Length; i++)
+            textContent.text = "";    // 清除上次對話內容
+            goTip.SetActive(false);   // 隱藏三角形圖示
+
+            for (int i = 0; i < test[j].Length; i++)   // 遍尋對話中的每一個字
             {
                 textContent.text += test[j][i];    // 疊加對話內容文字介面
                 yield return new WaitForSeconds(interval);
             }
+
+            goTip.SetActive(true);
+
+            while (!Input.GetKeyDown(keyDialogue))
+            {
+                yield return null;
+            }
         }
+        goDialogue.SetActive(false);
     }
 }
