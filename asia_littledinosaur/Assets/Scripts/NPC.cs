b4fe529fa34b7c3e0aa4155e2a834b3b1610b748
immortalimpostor9634 <90890242+immortalimpostor9634@users.noreply.github.com>
2021-12-17 15:56:35 +0800

    碰撞設定前置

---
+++ asia_littledinosaur/Assets/Scripts/NPC.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+/// <summary>
+/// NPC行為
+/// 偵測目標進入碰撞區
+/// 顯示對話系統
+/// </summary>
+public class NPC : MonoBehaviour
+{
+    [Header("對話資料")]
+    public Datadialogue datadialogue;
+    [Header("對話系統")]
+    public Dialoguesystem Dialoguesystem;
+}
